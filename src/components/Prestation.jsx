40f69b78e72a380308ebb0b817ee4cc4374639af
FrançoisB <supervoigi@gmail.com>
2024-11-22 21:47:11 +0100

    update prestations

--- src/components/Prestation.jsx
+++ src/components/Prestation.jsx
@@ -3,29 +3,29 @@ import './Prestation.css'
 
 const Prestation = () => {
   return (
-    <div id='prestations'>
-        <div id='prestations_principales'>
-          <div>
-              <h2>Prestation 1</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
-          </div>
-          <div>
-              <h2>Prestation 2</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
-          </div>
-      </div>
-      <div id='prestations_secondaires'>
-      <div>
-              <h2>Prestation 3</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
-          </div>
-          <div>
-              <h2>Prestation 4</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
-          </div>      
+    <div id="prestations">
+    <div id="prestations_principales">
+        <div class="prestation">
+            <h2>Prestation 1</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
+        </div>
+        <div class="prestation">
+            <h2>Prestation 2</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
+        </div>
     </div>
+    <div id="prestations_secondaires">
+        <div class="prestation">
+            <h2>Prestation 3</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
+        </div>
+        <div class="prestation">
+            <h2>Prestation 4</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
+        </div>
     </div>
-    
+</div>
+
     
     
   )
