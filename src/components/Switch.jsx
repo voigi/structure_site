b23098e3d6978364612469dd67d63949d6f0fa04
FrançoisB <38584907+voigi@users.noreply.github.com>
2024-11-23 09:59:10 +0100

    Update components

---
+++ src/components/Switch.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Switch = () => {
+  return (
+    <div className="form-check form-switch float-end mt-2 me-2 d-flex justify-content-end">
+      <input className="form-check-input" style={{width: '60px', height: '30px',position: 'relative', left: '42vw', bottom: '0.5vh'}} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+      <label className="form-check-label" for="flexSwitchCheckDefault">
+       
+      </label>
+    </div>
+  )
+}
+
+export default Switch
+
+
