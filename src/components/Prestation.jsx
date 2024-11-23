b66b23d4bc25ff7b6df827267e0c7a8520094f60
FrançoisB <supervoigi@gmail.com>
2024-11-23 16:29:36 +0100

    update prestations

--- src/components/Prestation.jsx
+++ src/components/Prestation.jsx
@@ -19,10 +19,10 @@ const Prestation = () => {
             <h2>Prestation 3</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
         </div>
-        <div class="prestation">
+        {/* <div class="prestation">
             <h2>Prestation 4</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor arcu eu arcu pulvinar, sed suscipit velit consectetur. Nulla facilisi. Quisque non odio vel velit gravida faucibus. In hac habitasse platea dictumst.</p>
-        </div>
+        </div> */}
     </div>
 </div>
 
