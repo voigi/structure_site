348c52ea141eda1d47f18494bbe353c26b7b8f7c
FrançoisB <supervoigi@gmail.com>
2024-11-24 18:10:32 +0100

    Update switch

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx';
-import './components/Switch.js';
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
