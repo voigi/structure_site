0fa77e80d275e35ba77744a38e037f23e4f432c0
FrançoisB <supervoigi@gmail.com>
2024-11-24 18:08:11 +0100

    update switch

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import './Switch.css'
-
+import React, { useState } from 'react';
+import './Switch.css';
 
 const Switch = () => {
+  // Gère l'état du thème
+  const [isDark, setIsDark] = useState(false);
+
+  // Gestionnaire pour le changement du switch
+  const handleChange = () => {
+    const newTheme = !isDark; // Inverse l'état actuel
+    setIsDark(newTheme); // Met à jour l'état
+    document.body.setAttribute('data-bs-theme', newTheme ? 'dark' : 'light'); // Définit le thème
+  };
+
   return (
     <div className="form-check form-switch float-end mt-2 me-2 d-flex justify-content-end">
-      <input className="form-check-input" style={{width: '60px', height: '30px',position: 'relative', left: '42vw', bottom: '0.5vh'}} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-      <label className="form-check-label" for="flexSwitchCheckDefault">
-       
-      </label>
+      <input
+        className="form-check-input"
+        style={{ width: '60px', height: '30px', position: 'relative', left: '42vw', bottom: '0.5vh' }}
+        type="checkbox"
+        role="switch"
+        id="flexSwitchCheckDefault"
+        checked={isDark}
+        onChange={handleChange} // Événement React
+      />
+      <label className="form-check-label" htmlFor="flexSwitchCheckDefault"></label>
     </div>
-  )
-}
+  );
+};
 
-export default Switch
+export default Switch;
+
 
 
